41891ef60a4baecf240cc3601f6509f82439eba9
Piotr Przekaza <piotr.przekaza@hotmail.com>
2019-04-15 19:48:18 +0200

    Updates

---
+++ precache-manifest.23415ada487e5e895a187002861bbd9d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/SPA/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "607bedc87574f72b2daf",
+    "url": "/SPA/static/css/main.791268c1.chunk.css"
+  },
+  {
+    "revision": "7e86836b4faa091061c1",
+    "url": "/SPA/static/js/runtime~main.67f752b6.js"
+  },
+  {
+    "revision": "43f9118109871ada9347",
+    "url": "/SPA/static/js/2.4c84e522.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/SPA/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "607bedc87574f72b2daf",
+    "url": "/SPA/static/js/main.49460cc4.chunk.js"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/SPA/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "0c36a9ae176d953bfc8801af1910a4ae",
+    "url": "/SPA/static/media/molo.0c36a9ae.jpg"
+  },
+  {
+    "revision": "f547c96552de45276cf07ce15791f915",
+    "url": "/SPA/static/media/binball.f547c965.jpg"
+  },
+  {
+    "revision": "923824832d1a18abcd69e247ef5d5256",
+    "url": "/SPA/static/media/keyb.92382483.jpg"
+  },
+  {
+    "revision": "5eb299956d2eb8bf7d5f0013bfe71058",
+    "url": "/SPA/index.html"
+  }
+];
